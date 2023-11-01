8565c6436231ab76d51acd5059abb810079afb32
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-11-01 18:37:01 +0330

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,2 @@
+input("Enter your name ?")
+print(1,2,3,10,20,30)
---
+++ 3.py
@@ -0,0 +1,8 @@
+zel = input("Zel ra vared konid : ")
+zel = int(zel)
+
+mohit = zel * 4
+masahat = zel ** 2
+
+print(mohit)
+print(masahat)
